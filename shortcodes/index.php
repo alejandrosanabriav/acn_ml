3505b7d7152b1b70d1caee665df3409e091ceb35
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-03 15:51:43 -0500

    FIX ERROR

--- shortcodes/index.php
+++ shortcodes/index.php
@@ -1,6 +1,10 @@
 <?php
-include 'lib/shortcode_factory.php';
+include str_replace('shortcodes', '',  __DIR__) . 'lib/shortcode_factory.php';
+
 $base = __DIR__;
 
-//register shortcodes
+if(function_exists('sc_factory')) {
+  //register shortcodes
 sc_factory('test_sc', array(), $base . 'test.php');
+
+}
