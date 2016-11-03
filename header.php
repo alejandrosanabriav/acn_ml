7a7fdd2e6a429849550553acaa25a254806ce083
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-03 11:31:59 -0500

    get logo

--- header.php
+++ header.php
@@ -3,5 +3,23 @@
 <head>
   <meta charset="UTF-8">
   <title>ACN MD</title>
+  
+  <style>
+    #header {
+      height: 100px;
+      width: 100%;
+      background: #fff;
+    }
+
+     #header img {
+       float: left;
+       margin: 40px 0 0 40px;
+     }
+  </style>
+
 </head>
 <body>
+
+<div id="header">
+  <img src="<?php echo get_template_directory_uri() ?>/public/logos/logo.png" alt="">
+</div>
