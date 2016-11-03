a39532a47f17ab7a0531987eaf99135ecd964ec2
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-03 18:35:39 -0500

    validate all

--- client/form.js
+++ client/form.js
@@ -1,13 +1,30 @@
 'use strict';
 import $ from 'jquery';
+import validator from 'validator';
+
+function validate(type, val) {
+  switch(type) {
+    case 'required':
+      return !validator.isEmpty(val);
+    break;
+    default: 
+      return false;
+  }
+}
 
 export default function() {
   $('form[data-validate="true"]').on('submit', function(e) {
     e.preventDefault();
     let inputs = $(this).find('input');
     inputs.each(function(index) {
-      console.log($(this).data('validate').split('|'));
-      console.log($(this).data('messages').split('|'));
+      let val = $(this).val();
+      let validations = $(this).data('validate').split('|');
+      let messages = $(this).data('messages').split('|');
+      
+      validations.forEach(type => {
+        console.log(validate(type, val));
+      });
+
     });
 
   });
