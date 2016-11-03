b53798a7424108c993c1f19882ddbf172abb2c8c
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-03 17:37:24 -0500

    add normalize

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
    <?php wp_head(); ?>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
    <link href="https://fonts.googleapis.com/css?family=Rubik:300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/public/css/base.css">
 </head>
 <body>
