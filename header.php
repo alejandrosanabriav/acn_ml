75c2888456f9de92c3fb2b36e0949df916ca902b
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-03 17:31:45 -0500

    add more font styles

--- header.php
+++ header.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
- <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
 
   <title>ACN ML</title>
    <?php wp_head(); ?>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+   <link href="https://fonts.googleapis.com/css?family=Rubik:300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/public/css/base.css">
 </head>
 <body>
