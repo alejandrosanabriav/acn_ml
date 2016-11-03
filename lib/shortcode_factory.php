5b79858f3515803dbeaf34ca1a6d0fd879e4e648
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-03 15:56:59 -0500

    trynna debug

--- lib/shortcode_factory.php
+++ lib/shortcode_factory.php
@@ -8,7 +8,7 @@ function sc_factory($sc_name, $sc_atts = array(), $template_uri) {
     if(file_exists($template_uri)) {
       include $template_uri;
     } else {
-      echo 'template not found';
+      echo 'template not found: ' . $template_uri;
     }
     
     return ob_get_clean();
