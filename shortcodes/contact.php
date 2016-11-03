e2d776e2854043640f20380c5fa6fe255dc6408c
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-03 17:15:38 -0500

    mo atts to button form

--- shortcodes/contact.php
+++ shortcodes/contact.php
@@ -1,6 +1,6 @@
 <form action="">
   <?php echo do_shortcode($content) ?>
   
-  <button class="btn btn--outline"><?php echo $at['button_title'] ?></button>
+  <button class="btn btn--outline" style="<?php echo $at['btn_style'] ?>"><?php echo $at['btn_title'] ?></button>
 </form>
 
