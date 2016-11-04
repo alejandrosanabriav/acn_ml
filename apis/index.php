2abe9974283b3e95284c8ab5e928bdefa866425c
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-04 14:55:10 -0500

    fix get api

--- apis/index.php
+++ apis/index.php
@@ -6,7 +6,7 @@ add_action( 'wp_ajax_mailchimp_subscribe', 'mailchimp_subscribe' );
 function mailchimp_subscribe() {
   $data = $_POST['data'];
   $listId = get_option('mailchimp_list_id');
-  $apiKey = get_option('mailchimp_api_key');
+  $apiKey =  get_option('mailchimp_api');
   // $res = mc_subscribe($data, $listId, $apiKey); ¸
   echo json_encode($apiKey);
   die();
