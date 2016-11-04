b5a89ab5830a968ea02481adaa5d5342cce6a6ab
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-04 14:17:38 -0500

    alright

--- options/accounts.php
+++ options/accounts.php
@@ -23,12 +23,16 @@ function bs_accounts_settings() {
 }
 
 function bs_accounts_options() {
+
 ?>
 <div style="padding: 15px; margin: 15px; background: #fff; box-shadow: 1px 1px 5px rgba(0,0,0, .1);">
 
   <h1>Accounts keys</h1>
   <p></p>
   <form method="post" action="options.php" style="position: relative; margin: 0 auto">
+    <?php settings_fields( 'bs_accounts_info_group' ); ?>
+    <?php do_settings_sections( 'bs_accounts_info_group' ); ?>
+    
     <p>
         <input 
           type="text"
