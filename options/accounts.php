cd876e674634b45579cf3df390b92f9033cf0275
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-04 14:01:37 -0500

    crowd

--- options/accounts.php
+++ options/accounts.php
@@ -18,21 +18,19 @@ function bs_admin_accounts_options_menu() {
 
 function bs_accounts_settings() {
   register_setting( 'bs_accounts_info_group', 'mailchimp_api' );
-  register_setting( 'bs_accounts_info_group', 'stripe_key_' );
-  register_setting( 'bs_accounts_info_group', 'stripe_api' );
-
+  register_setting( 'bs_accounts_info_group', 'stripe_key_private' );
+  register_setting( 'bs_accounts_info_group', 'stripe_key_public' );
 }
 
 function bs_accounts_options() {
 ?>
-<div>
-<div style="text-align: center; text-shadow: 1px 1px 3px rgba(0,0,0, .1)">
-			<h1>Offices</h1>
-		</div>
+<div style="background: #f1f1f1; background-size: contain; padding: 15px">
+
+<div style="text-align: center; text-shadow: 1px 1px 3px rgba(0,0,0, .1)"><h1>Offices</h1></div>
     
-    <form method="post" action="options.php" style="position: relative; width: 80%; margin: 0 auto">
-      <input type="text" placeholder="api key">
-    </form>
+  <form method="post" action="options.php" style="position: relative; width: 80%; margin: 0 auto">
+    <input type="text" placeholder="api key">
+  </form>
 </div>
 
 
