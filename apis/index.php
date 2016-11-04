564dcaa94b4e4d47adda8375e04fa561cd738061
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-04 14:54:23 -0500

    I will survive

--- apis/index.php
+++ apis/index.php
@@ -6,8 +6,8 @@ add_action( 'wp_ajax_mailchimp_subscribe', 'mailchimp_subscribe' );
 function mailchimp_subscribe() {
   $data = $_POST['data'];
   $listId = get_option('mailchimp_list_id');
-  $apiKey= get_option('mailchimp_api_key');
-  $res = mc_subscribe($data, $listId, $apiKey); 
-  echo json_encode($res);
+  $apiKey = get_option('mailchimp_api_key');
+  // $res = mc_subscribe($data, $listId, $apiKey); ¸
+  echo json_encode($apiKey);
   die();
 }
