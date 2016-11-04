17ad533cca4e8fe1dab8662d2be51d93e7d0796a
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-04 17:27:54 -0500

    add colors to tiny mce

--- client/donate.js
+++ client/donate.js
@@ -20,7 +20,6 @@ export default function donate() {
       console.log(this);
     })($amountBtns);
 
-
   })($forms);
   
 }
--- functions.php
+++ functions.php
@@ -8,3 +8,57 @@
 include_once 'shortcodes/index.php';
 include_once 'options/index.php';
 include_once 'apis/index.php';
+
+function my_mce4_options( $init ) {
+$default_colours = '
+	"000000", "Black",
+	"993300", "Burnt orange",
+	"333300", "Dark olive",
+	"003300", "Dark green",
+	"003366", "Dark azure",
+	"000080", "Navy Blue",
+	"333399", "Indigo",
+	"333333", "Very dark gray",
+	"800000", "Maroon",
+	"FF6600", "Orange",
+	"808000", "Olive",
+	"008000", "Green",
+	"008080", "Teal",
+	"0000FF", "Blue",
+	"666699", "Grayish blue",
+	"808080", "Gray",
+	"FF0000", "Red",
+	"FF9900", "Amber",
+	"99CC00", "Yellow green",
+	"339966", "Sea green",
+	"33CCCC", "Turquoise",
+	"3366FF", "Royal blue",
+	"800080", "Purple",
+	"999999", "Medium gray",
+	"FF00FF", "Magenta",
+	"FFCC00", "Gold",
+	"FFFF00", "Yellow",
+	"00FF00", "Lime",
+	"00FFFF", "Aqua",
+	"00CCFF", "Sky blue",
+	"993366", "Brown",
+	"C0C0C0", "Silver",
+	"FF99CC", "Pink",
+	"FFCC99", "Peach",
+	"FFFF99", "Light yellow",
+	"CCFFCC", "Pale green",
+	"CCFFFF", "Pale cyan",
+	"99CCFF", "Light sky blue",
+	"CC99FF", "Plum",
+	"FFFFFF", "White"
+	';
+$custom_colours = '
+	"FC3938", "red Orange",
+	"334754", "Oxford",
+	';
+$init['textcolor_map'] = '['.$default_colours.','.$custom_colours.']';
+$init['textcolor_rows'] = 6; // expand colour grid to 6 rows
+return $init;
+}
+
+add_filter('tiny_mce_before_init', 'my_mce4_options');
