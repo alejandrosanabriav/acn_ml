cbe2dad780d653fb43da4d7a049b8a5e5d2ade58
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-04 15:00:44 -0500

    encode data

--- apis/index.php
+++ apis/index.php
@@ -4,7 +4,7 @@ add_action( 'wp_ajax_nopriv_mailchimp_subscribe', 'mailchimp_subscribe' );
 add_action( 'wp_ajax_mailchimp_subscribe', 'mailchimp_subscribe' );
 
 function mailchimp_subscribe() {
-  $data = $_POST['data'];
+  $data = json_encode($_POST['data']);
   $listId = get_option('mailchimp_list_id');
   $apiKey =  get_option('mailchimp_api');
   $res = mc_subscribe($data, $listId, $apiKey);
