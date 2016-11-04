d705607cfae481c4b895211f389f2a4d7906e50e
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-04 14:42:49 -0500

    encode resonse

--- apis/index.php
+++ apis/index.php
@@ -4,6 +4,6 @@ add_action( 'wp_ajax_nopriv_mailchimp_subscribe', 'mailchimp_subscribe' );
 add_action( 'wp_ajax_mailchimp_subscribe', 'mailchimp_subscribe' );
 
 function mailchimp_subscribe() {
-  echo $_POST['data'];
+  echo json_encode($_POST['data']);
   die();
 }
