e6a0796737219718d863eb89551ace9b119463b2
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-07 11:35:54 -0500

    get maxStep

--- client/donate.js
+++ client/donate.js
@@ -3,5 +3,7 @@ import $ from 'jquery';
 
 export default function() {
   let form = document.querySelector('.bs_donate_form');
-  console.log(form.querySelectorAll('.bs_donate_form__step').length);
+  let steps = form.querySelectorAll('.bs_donate_form__step');
+  let maxStep = steps.length;
+  console.log(maxStep);
 }
