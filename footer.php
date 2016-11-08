4568397586782bbd65a2a5cf2999e7be92832869
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-08 11:28:50 -0500

    fix bootstrap

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.28/vue.min.js"></script>
 <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
 <script src="<?php echo get_template_directory_uri() . '/public/js/app.js' ?>"></script>
