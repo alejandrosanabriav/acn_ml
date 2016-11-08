ebf94fa729184c23443f8d6f0a5e3d4c6975a9c0
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-08 12:29:50 -0500

    meta viewport

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="UTF-8">
-
+  <meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1, user-scalable=0">
   <title>ACN ML</title>
    <?php wp_head(); ?>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
