b25408d30691e68b0098614255403648379639ed
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-08 11:26:12 -0500

    bear with me

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.28/vue.min.js"></script>
+<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
 <script src="<?php echo get_template_directory_uri() . '/public/js/app.js' ?>"></script>
 
 <?php wp_footer() ?>
