5bded822a6848fbc29b21d399ea341dd274f4b9e
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-08 12:04:10 -0500

    add btns

--- shortcodes/donate.php
+++ shortcodes/donate.php
@@ -8,11 +8,11 @@
 
   <template id="change-amount-template">
     <ul class="change-amount" style="padding: 0 15px">
-      <li><a href="#" @click="changeAmount(10, $event)">$10</a></li>
-      <li><a href="#" @click="changeAmount(30, $event)">$30</a></li>
-      <li><a href="#" @click="changeAmount(50, $event)">$50</a></li>
-      <li><a href="#" @click="changeAmount(100, $event)">$100</a></li>
-      <li><a href="#" @click="changeAmount('', $event)"><?php echo gett('Other') ?></a></li>
+      <li><button @click="changeAmount(10, $event)">$10</button></li>
+      <li><button @click="changeAmount(30, $event)">$30</button></li>
+      <li><button @click="changeAmount(50, $event)">$50</button></li>
+      <li><button @click="changeAmount(100, $event)">$100</button></li>
+      <li><button @click="changeAmount('', $event)"><?php echo gett('Other') ?></button></li>
     </ul>
   </template>
 
@@ -57,10 +57,6 @@
           </div>
 
         <button class="donate_landing__submit" @click.prevent="nextSection">
-
-          <svg width="47px" height="42px" viewBox="-3 12 47 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-              <path d="M36.9462953,18.3948561 C35.920883,17.3453321 34.6973083,16.4970205 33.3463434,15.9110899 C31.9890876,15.326769 30.5012962,15 28.9348689,15 C27.3747325,15 25.8885138,15.4024249 24.5296853,16.1123023 C23.1787204,16.8173507 21.9567184,17.8298516 20.9297334,19.0371262 C20.6482169,19.3719437 20.3824275,19.7260776 20.1307926,20.0882599 C19.8791577,19.7244679 19.614941,19.3719437 19.3334245,19.0371262 C18.3080122,17.8298516 17.0844375,16.8173507 15.7334726,16.1123023 C14.3762168,15.4024249 12.8884254,15 11.3219981,15 C9.76186169,15 8.26935214,15.326769 6.91681452,15.9110899 C5.55955873,16.4970205 4.34384759,17.3453321 3.31686262,18.3948561 C2.29145038,19.4459898 1.4626279,20.6902875 0.890158485,22.0794581 C0.319261789,23.4637996 0,24.9914044 0,26.5882263 C0,28.1914869 0.319261789,29.7142626 0.890158485,31.1034332 C1.4626279,32.486165 2.29145038,33.7385112 3.31686262,34.7880352 C4.34384759,35.8311204 18.7751095,49.1803579 20.1323653,49.7727273 C21.4833302,49.1803579 35.920883,35.8311204 36.9462953,34.7880352 C37.9654166,33.7385112 38.7958118,32.486165 39.3729994,31.1034332 C39.9438961,29.7142626 40.2631579,28.1914869 40.2631579,26.5882263 C40.2631579,24.9914044 39.9438961,23.4637996 39.3729994,22.0794581 C38.7958118,20.6902875 37.9654166,19.4459898 36.9462953,18.3948561 Z" id="Page-1" stroke="#FC3938" stroke-width="6" fill="none"></path>
-          </svg>
           <?php echo gett('Donate') ?>
         </button>
 
@@ -126,6 +122,7 @@
           placeholder="<?php echo gett('CVC') ?>"
           >
         </div>
+
         <div class="col-md-12">
           <div class="donate_landing__alert-danger alert alert-danger" v-if="errors">
             <span v-if="errors.amount"><?php echo gett('Amount') . ' ' . gett('required') ?>, </span>
