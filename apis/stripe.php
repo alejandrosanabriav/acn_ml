0866bbc501a6a8c1118e9081758316907c82fa46
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-08 14:03:44 -0500

    get exception if any err

--- apis/stripe.php
+++ apis/stripe.php
@@ -4,8 +4,8 @@ require $dir_base . 'vendor/autoload.php';
 
 function stripe_create_token($api_key, $card) {
   \Stripe\Stripe::setApiKey($api_key);
-
-  $token = \Stripe\Token::create(array(
+  try {
+      $token = \Stripe\Token::create(array(
     "card" => array(
       "number" => $card['number'],
       "exp_month" => $card['exp_month'],
@@ -15,15 +15,23 @@ function stripe_create_token($api_key, $card) {
   ));
 
   return $token;
+  } catch(Exception $e) {
+    return $e;
+  }
+
 }
 
 function stripe_create_customer($api_key, $customer) {
   \Stripe\Stripe::setApiKey($api_key);
-
-  $customer = \Stripe\Customer::create(array(
-    "email" => $customer['email'],
-    "source" => $customer['token']
-  ));
+  try {
+    $customer = \Stripe\Customer::create(array(
+      "email" => $customer['email'],
+      "source" => $customer['token']
+    ));
+  } catch(Exception $e) {
+    return $e;
+  }
+ 
 
   return $customer;
 }
