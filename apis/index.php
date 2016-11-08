013cf88f9f19cb40d4c683c6688fca1ed4fc5aba
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-08 14:58:59 -0500

    fix that error

--- apis/index.php
+++ apis/index.php
@@ -25,15 +25,3 @@ function stripe_token() {
   echo json_encode($res);
   die();
 }
-
-add_action( 'wp_ajax_nopriv_stripe_token', 'stripe_token' );
-add_action( 'wp_ajax_stripe_token', 'stripe_token' );
-
-function stripe_token() {
-  $card = $_POST['data'];
-  $apiKey =  get_option('stripe_key_private');
-  $res = stripe_create_token( $apiKey, $card);
-  header('Content-type: application/json');
-  echo json_encode($res);
-  die();
-}
