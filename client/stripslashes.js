6b983a4a960b46d477e13ee6f571677501522c6d
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-17 15:13:32 -0500

    stripslashes

---
+++ client/stripslashes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+export default str => {
+	return (str + '')
+    .replace(/\\(.?)/g, function (s, n1) {
+      switch (n1) {
+        case '\\':
+          return '\\'
+        case '0':
+          return '\u0000'
+        case '':
+          return ''
+        default:
+          return n1
+      }
+    })
+}
