539faa00c2ed82489bf339b80d9c7f54172d1d50
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-17 15:08:53 -0500

    encode url

--- shortcodes/slider_bg.php
+++ shortcodes/slider_bg.php
@@ -2,7 +2,7 @@
  $imagesArr = explode(',', $at['images']);
  
  function getImagesSrc($imageId) {
-	 return  wp_get_attachment_image_src($imageId, 'full')[0];
+	 return  urlencode(wp_get_attachment_image_src($imageId, 'full')[0]);
  }
 
  $images = array_map('getImagesSrc', $imagesArr);
