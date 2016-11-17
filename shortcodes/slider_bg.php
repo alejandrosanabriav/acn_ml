3378f41b0040dfa680e964d05d10b56855026c39
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-17 15:04:54 -0500

    string

--- shortcodes/slider_bg.php
+++ shortcodes/slider_bg.php
@@ -8,5 +8,5 @@
  $images = array_map('getImagesSrc', $imagesArr);
 ?>
 
-<bsslider-bg :images="<?php echo json_encode($images) ?>"></bsslider-bg>
+<bsslider-bg images="<?php echo json_encode($images) ?>"></bsslider-bg>
 <?php var_dump(json_encode($images)); ?>
