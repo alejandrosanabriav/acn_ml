30b51e4deac3924e48f5a5db5222f4fdd2446262
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-17 15:11:41 -0500

    slashes

--- shortcodes/slider_bg.php
+++ shortcodes/slider_bg.php
@@ -2,11 +2,11 @@
  $imagesArr = explode(',', $at['images']);
  
  function getImagesSrc($imageId) {
-	 return  urldecode(wp_get_attachment_image_src($imageId, 'full')[0]);
+	 return  wp_get_attachment_image_src($imageId, 'full')[0];
  }
 
  $images = array_map('getImagesSrc', $imagesArr);
 ?>
 
-<bsslider-bg images="<?php echo json_encode($images) ?>"></bsslider-bg>
+<bsslider-bg images="<?php echo json_encode($images, JSON_UNESCAPED_SLASHES) ?>"></bsslider-bg>
 <?php var_dump(json_encode($images)); ?>
