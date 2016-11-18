d41dc44805d738a83448494832ac4dea0eda98ff
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-18 18:17:12 -0500

    get amount

--- client/components/donate.js
+++ client/components/donate.js
@@ -230,7 +230,7 @@ export default () => ({
 
 		onSubmit(e) {
 			const { contact, currency, amount, donation_type, stripe_token } = this;
-			console.log(contact);
+			console.log('amount', amount);
 			let data = { ...contact, currency, amount, donation_type, stripe_token}; 
 			e.preventDefault();
 			this.contactValidations();
