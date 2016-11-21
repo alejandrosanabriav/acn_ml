7d9819d96502ce3ed0341e7fe28f9be4371253f9
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-21 14:57:15 -0500

    remove another donate

--- client/index.js
+++ client/index.js
@@ -2,14 +2,13 @@
 import input from './input';
 import form from './form';
 import donate from './components/donate';
-import donateLand from './components/donate_land';
+
 import changeAmount from './components/change_amount';
 import sliderBg from './components/slider_bg';
 
 (function() {
 	Vue.component('change-amount', changeAmount());
 	Vue.component('donate-landing', donate());
-	Vue.component('donate-land', donateLand());
 	Vue.component('bsslider-bg', sliderBg());
 
 	const vm = new Vue({
