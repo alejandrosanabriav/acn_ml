d120f5b599dfc7fd7af91c60d5aa4749509b626d
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-20 20:12:19 -0500

    jquery

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
    
    <?php wp_head(); ?>
 
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
    <link href="https://fonts.googleapis.com/css?family=Rubik:300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/public/css/base.css">
