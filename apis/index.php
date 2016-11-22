a2507ec798617ac697269567857a2f3d8c5b024c
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-22 18:32:36 -0500

    add tags

--- apis/index.php
+++ apis/index.php
@@ -65,7 +65,35 @@ function infusion_contact() {
   $data = $_POST['data'];
   $key = get_option('infusionsoft_key');
   $subdomain = get_option('infusionsoft_subdomain');
+  $countryTags = [
+    'Australia' => '822',
+    'Austria' => '824',
+    'Belgium' => '826',
+    'Brazil' => '828',
+    'Canada' => '830',
+    'Chile' => '832',
+    'Colombia' => '834',
+    'France' => '836',
+    'Germany' => '838',
+    'Ireland' => '840',
+    'Italy' => '842',
+    'Malta' => '844',
+    'Mexico' => '846',
+    'Netherlands' => '848',
+    'Philippines' => '850',
+    'Poland' => '852',
+    'Portugal' => '854',
+    'Slovakia' => '856',
+    'Republic of Korea' => '858',
+    'Spain' => '860',
+    'Switzerland' => '862'
+  ];
+
+  $countryTag = array_key_exists($data['country'], $countryTags) ? $countryTags[$data['country']] : [];
+
+  $defaultTags = [800, 802];
   $tags = explode(',', get_option('infusionsoft_tags'));
+  $tags = array_push($tags, $defaultTags, $countryTag);
   $infusionsoft = new Infusionsoft($subdomain, $key);
   $name = explode(" ", $data['name']);
 
