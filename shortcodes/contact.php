2c82d139085a0d8cbaf7dfed7df1c135f34b64d4
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-22 10:24:29 -0500

    fix url

--- shortcodes/contact.php
+++ shortcodes/contact.php
@@ -2,7 +2,7 @@
   :texts="{pray: 'PRAY'}" 
   :placeholders="{name: 'Name', email: 'Email'}" 
   country="Colombia"
-  baseUri="<?php echo get_template_dir_uri() ?>"
+  baseUri="<?php echo get_template_directory_uri() ?>"
 >
 
 </contact>
