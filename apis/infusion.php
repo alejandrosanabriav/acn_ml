096bdd547784ed614022b66ca24d95305c3b5945
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-22 15:23:37 -0500

    get file

--- apis/infusion.php
+++ apis/infusion.php
@@ -1,12 +1,11 @@
 <?php
 $dir_base =  str_replace('apis', '', __DIR__);
-require $dir_base . 'vendor/autoload.php';
+require $dir_base . '/vendor/autoload.php';
 
 function infusion_create_contact() {
+  try{
   $app = new iSDK('d871db40497cbbd7c9e25898749d128d');
   $contactData = array('FirstName' => 'John', 'LastName'  => 'Doe', 'Email' => 'JDoe@email.com');
-
-  try{
     $res = $app->addCon($contactData);
     return $res;
   } catch(Exception $e) {
