4265b9d3b50d56ee7deab5741664ecdf398fe529
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-21 20:09:48 -0500

    add more

--- apis/infusion.php
+++ apis/infusion.php
@@ -2,8 +2,15 @@
 $dir_base =  str_replace('apis', '', __DIR__);
 require $dir_base . 'vendor/autoload.php';
 
-$infusionsoft = new \Infusionsoft\Infusionsoft(array(
-    'clientId'     => 'XXXXXXXXXXXXXXXXXXXXXXXX',
-    'clientSecret' => 'XXXXXXXXXX',
-    'redirectUri'  => 'http://example.com/',
-));
+function create_contact() {
+  $app = new iSDK();
+  $contactData = array('FirstName' => 'John', 'LastName'  => 'Doe', 'Email' => 'JDoe@email.com');
+
+  try{
+    $res = $app->addCon('d871db40497cbbd7c9e25898749d128d', $contactData);
+    return $res;
+  } catch(Exception $e) {
+    return $e;
+  }
+ 
+}
