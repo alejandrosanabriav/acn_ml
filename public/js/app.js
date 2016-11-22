ed6d4fdb3e6a2465b425082b119cca75f5e4faef
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-21 19:01:24 -0500

    tryna get in

--- public/js/app.js
+++ public/js/app.js
@@ -13656,8 +13656,7 @@
 					var val = this.$get('contact.' + field);
 
 					if (field == 'email' && val) {
-						console.log(_validator2.default.isEmail(val));
-						this.$set('errors.' + field, !_validator2.default.isEmail(val));
+						this.$set('errors[\'contact.' + field + '\']', !_validator2.default.isEmail(val));
 					}
 
 					this.$set('errors[\'contact.' + field + '\']', _validator2.default.isEmpty(val));
