347aba40a35abcd493dba2775d3e63abf1076543
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-23 11:35:52 -0500

    wp title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1, user-scalable=0">
-  <title>ACN ML</title>
+  <title><?php wp_title(); ?></title>
    
    <?php wp_head(); ?>
 
