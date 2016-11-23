586f96aed04d7f0c867236b0971246337e21bdcc
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-22 19:44:21 -0500

    get country

--- shortcodes/contact.php
+++ shortcodes/contact.php
@@ -1,7 +1,7 @@
 <contact 
   :texts="{pray: 'PRAY', checkbox: 'I accept to receive updates and information from ACN'}" 
   :placeholders="{name: 'Name', email: 'Email'}" 
-  country="Colombia"
+  country="<?php echo getCountry() ?>"
   base-uri=<?php echo get_template_directory_uri() ?>
   redirect=<?php echo get_option('subscribe_redirect') ?>
 >
