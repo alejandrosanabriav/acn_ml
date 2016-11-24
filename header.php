fe3a243b61899b6f53a51691d9d90fbd9a44c9ba
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-24 00:05:07 -0500

    add og image

--- header.php
+++ header.php
@@ -3,9 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1, user-scalable=0">
+  <meta property="og:image" content="http://acninternational.org/wp-content/uploads/sites/4/2016/11/slider1.jpg" />
+  
   <title><?php wp_title(); ?></title>
-   
-   <?php wp_head(); ?>
+
+  <?php wp_head(); ?>
 
   <link href="https://fonts.googleapis.com/css?family=Rubik:300,300i,400,400i,500,500i" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
