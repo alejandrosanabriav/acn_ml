8d5c77043baa486d6512a1fe3d9187c9a5a937d7
Alejandro Sanabria <alesanabriav@gmail.com>
2016-11-25 10:22:01 -0500

    res

--- client/components/contact.js
+++ client/components/contact.js
@@ -85,7 +85,7 @@ export default () => {
 						type: 'post',
 						url: '/wp-admin/admin-ajax.php',
 						data: data
-					}).then(res => console.log(res)); //window.location = this.redirect
+					}).then(res => console.log('res', res)); //window.location = this.redirect
 				}
 		
 			}
