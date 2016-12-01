46136b06ac1ebba963b8cd0003401e992b4b2b96
Alejandro Sanabria <alesanabriav@gmail.com>
2016-12-01 11:09:27 -0500

    isset get ip

--- lib/get_ip.php
+++ lib/get_ip.php
@@ -2,7 +2,7 @@
 
 function get_client_ip_server() {
     $ipaddress = '';
-    if ($_SERVER['HTTP_CLIENT_IP'])
+    if (isset($_SERVER['HTTP_CLIENT_IP']) && $_SERVER['HTTP_CLIENT_IP'])
         $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
     else if($_SERVER['HTTP_X_FORWARDED_FOR'])
         $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
