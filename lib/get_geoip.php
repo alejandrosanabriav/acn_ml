1a8ef39a8354d76b848b9313cf0e1acffcabc74e
Alejandro Sanabria <alesanabriav@gmail.com>
2016-12-01 17:37:25 -0500

    unzip

--- lib/get_geoip.php
+++ lib/get_geoip.php
@@ -1,23 +1,8 @@
 <?php
 
-function geoip_db() {
-	try {
-			//download db
-			$dir_base =  str_replace('lib', '', __DIR__);
-			$url  = 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz';
-			$path =  $dir_base.'/GeoLite2-Country.mmdb.gz';
-			$ch = curl_init($url);
+function unzip_db($file_name) {
 
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-			$data = curl_exec($ch);
-
-			curl_close($ch);
-
-			file_put_contents($path, $data);
-			$file_name = $path;
-			
-			//unzip db
+		//unzip db
 			$buffer_size = 1000000; // read 4kb at a time
 			$out_file_name = str_replace('.gz', '', $file_name); 
 
@@ -36,6 +21,26 @@ function geoip_db() {
 			fclose($out_file);
 			gzclose($file);
 
+}
+
+function geoip_db() {
+	try {
+			//download db
+			$dir_base =  str_replace('lib', '', __DIR__);
+			$url  = 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz';
+			$path =  $dir_base.'/GeoLite2-Country.mmdb.gz';
+			$ch = curl_init($url);
+
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+			$data = curl_exec($ch);
+
+			curl_close($ch);
+
+			file_put_contents($path, $data);
+			$file_name = $path;
+			unzip_db($file_name);
+			
 		return true;
 	} catch(Exception $e) {
 		return $e->getMessage();
