cafa3c907d3e4485615aaed82e5129a3d192b08d
Alejandro Sanabria <alesanabriav@gmail.com>
2016-12-01 11:40:00 -0500

    dir bae

--- apis/location.php
+++ apis/location.php
@@ -1,9 +1,10 @@
 <?php
-$dir_base =  str_replace('apis', '', __DIR__);
+$dir_base = str_replace('apis', '', __DIR__);
 require $dir_base . '/vendor/autoload.php';
 use GeoIp2\Database\Reader;
 
-function get_location($ip) {	
+function get_location($ip) {
+	$dir_base = str_replace('apis', '', __DIR__);	
 	try {
 		
 		$reader = new Reader($dir_base .'/GeoLite2-Country.mmdb');
