c898f6d62363a67e04bb0f67fee7951d10a213fc
Alejandro Sanabria <alesanabriav@gmail.com>
2016-12-01 11:29:43 -0500

    remove .gz

--- apis/location.php
+++ apis/location.php
@@ -5,7 +5,7 @@ use GeoIp2\Database\Reader;
 
 function get_location($ip) {	
 	try {
-		$reader = new Reader($dir_base .'GeoLite2-Country.mmdb.gz');
+		$reader = new Reader($dir_base .'GeoLite2-Country.mmdb');
 		return $reader->city($ip);
 	} catch(Exception $e) {
 		return $e;
