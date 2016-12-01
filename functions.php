161d0c80b49b7b0997c94b2eb1ee3b91651b10cc
Alejandro Sanabria <alesanabriav@gmail.com>
2016-12-01 12:00:03 -0500

    get db to func

--- functions.php
+++ functions.php
@@ -121,38 +121,3 @@ function getImageSrc($imageId) {
 }
 
 
-if(!file_exists('GeoLite2-Country.mmdb.gz')) {
-	$url  = 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz';
-	$path =  __DIR__.'/GeoLite2-Country.mmdb.gz';
-
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-	$data = curl_exec($ch);
-
-	curl_close($ch);
-
-	file_put_contents($path, $data);
-
-$file_name = $path;
-
-// Raising this value may increase performance
-$buffer_size = 4096; // read 4kb at a time
-$out_file_name = str_replace('.gz', '', $file_name); 
-
-// Open our files (in binary mode)
-$file = gzopen($file_name, 'rb');
-$out_file = fopen($out_file_name, 'wb'); 
-
-// Keep repeating until the end of the input file
-while (!gzeof($file)) {
-    // Read buffer-size bytes
-    // Both fwrite and gzread and binary-safe
-    fwrite($out_file, gzread($file, $buffer_size));
-}
-
-// Files are done, close files
-fclose($out_file);
-gzclose($file);
-}
-
---
+++ lib/get_geoip.php
@@ -0,0 +1,41 @@
+<?php
+
+function geoip_db() {
+	$dir_base =  str_replace('lib', '', __DIR__);
+
+	if(!file_exists($dir_base.'GeoLite2-Country.mmdb.gz')) {
+		
+		$url  = 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz';
+		$path =  $dir_base.'/GeoLite2-Country.mmdb.gz';
+
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		$data = curl_exec($ch);
+
+		curl_close($ch);
+
+		file_put_contents($path, $data);
+
+	$file_name = $path;
+
+	// Raising this value may increase performance
+	$buffer_size = 4096; // read 4kb at a time
+	$out_file_name = str_replace('.gz', '', $file_name); 
+
+	// Open our files (in binary mode)
+	$file = gzopen($file_name, 'rb');
+	$out_file = fopen($out_file_name, 'wb'); 
+
+	// Keep repeating until the end of the input file
+	while (!gzeof($file)) {
+			// Read buffer-size bytes
+			// Both fwrite and gzread and binary-safe
+			fwrite($out_file, gzread($file, $buffer_size));
+	}
+
+	// Files are done, close files
+	fclose($out_file);
+	gzclose($file);
+	}
+}
