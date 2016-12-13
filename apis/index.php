ab9ba14046fb4499ac6a993d2e3052a0941e5d9f
Alejandro Sanabria <alesanabriav@gmail.com>
2016-12-13 10:26:30 -0500

    get apikey

--- apis/index.php
+++ apis/index.php
@@ -20,9 +20,9 @@ add_action( 'wp_ajax_mailchimp_subscribe', 'mailchimp_subscribe' );
 function mailchimp_subscribe() {
   $data = json_encode($_POST['data']);
   $listId = get_option('mailchimp_list_id');
-  $apiKey =  get_option('mailchimp_api');
+  $apiKey = get_option('mailchimp_api');
   $res = mc_subscribe($data, $listId, $apiKey);
-  echo $res;
+  echo $apiKey;
   die();
 }
 
