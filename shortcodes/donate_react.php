ef440cc2e0f0cae5304e84f0fc5f9b6a7519e9ce
Alejandro Sanabria <alesanabriav@gmail.com>
2016-12-21 17:54:18 -0500

    props

--- shortcodes/donate_react.php
+++ shortcodes/donate_react.php
@@ -1,9 +1,14 @@
 <?php
 function bs_donate_react_sc($atts, $content = null) {
+	 $at = shortcode_atts([
+		 "other" => "Otro",
+		 "monthly" => "Mensual",
+		 "once" => "Una vez"
+	 ]);
 	ob_start();
 ?>
 
-<div id="bs-donate-react" ></div>
+<div id="bs-donate-react" data-props="{other: <?php echo $at['other'] ?>}"></div>
 
 <?php
 return ob_get_clean();
