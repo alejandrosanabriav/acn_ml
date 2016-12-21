cdbaccbae606f5659aaae1cc42545b3ab2065141
Alejandro Sanabria <alesanabriav@gmail.com>
2016-12-21 17:21:34 -0500

    register shortcode

--- functions.php
+++ functions.php
@@ -15,6 +15,7 @@ include_once 'shortcodes/index.php';
 include_once 'shortcodes/slider.php';
 include_once 'shortcodes/donate.php';
 include_once 'shortcodes/donate_vertical.php';
+include_once 'shortcodes/donate_react.php';
 include_once 'shortcodes/contact.php';
 include_once 'options/index.php';
 include_once 'apis/index.php';
