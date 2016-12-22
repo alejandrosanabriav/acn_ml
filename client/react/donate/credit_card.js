816c9befedc1804074430ce0d92010e8cd91fd3a
Alejandro Sanabria <alesanabriav@gmail.com>
2016-12-22 16:36:59 -0500

    errors

--- client/react/donate/credit_card.js
+++ client/react/donate/credit_card.js
@@ -71,8 +71,7 @@ const CedritCard = React.createClass({
 		let exp_month = this.validateExpiry(stripe.exp_month, stripe.exp_year);
 		let exp_year = this.validateExpiry(stripe.exp_month, stripe.exp_year);
 		let cvc = this.validateCvc(stripe.cvc);
-		let errors = {errors: {...number, ...exp_month, ...exp_year, ...cvc}};
-		console.log(errors);
+		let errors = {...number, ...exp_month, ...exp_year, ...cvc};
 		this.props.onChange({errors});
 	},
 
