04923df64c94fbf5b449ef75acb3ca29a2e461c9
Alejandro Sanabria <alesanabriav@gmail.com>
2016-12-22 16:41:41 -0500

    spread

--- client/react/donate/credit_card.js
+++ client/react/donate/credit_card.js
@@ -71,7 +71,7 @@ const CedritCard = React.createClass({
 		let exp_month = this.validateExpiry(stripe.exp_month, stripe.exp_year);
 		let exp_year = this.validateExpiry(stripe.exp_month, stripe.exp_year);
 		let cvc = this.validateCvc(stripe.cvc);
-		let errors = {...number, ...exp_month, ...exp_year, ...cvc};
+		let errors = {...number, ...exp_month};
 		console.log(errors);
 		this.props.onChange({errors});
 	},
