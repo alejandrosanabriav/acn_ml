fe11c5a8b270b2da0621064bd45d767de973c7a7
Alejandro Sanabria <alesanabriav@gmail.com>
2016-12-22 16:32:04 -0500

    errors

--- public/js/app.js
+++ public/js/app.js
@@ -25711,8 +25711,7 @@
 			var exp_month = this.validateExpiry(stripe.exp_month, stripe.exp_year);
 			var exp_year = this.validateExpiry(stripe.exp_month, stripe.exp_year);
 			var cvc = this.validateCvc(stripe.cvc);
-			var er = { errors: _extends({}, number, exp_month, exp_year, cvc) };
-			var errors = _extends({}, this.props.errors, er);
+			var errors = { errors: _extends({}, number, exp_month, exp_year, cvc) };
 			console.log(errors);
 			this.props.onChange({ errors: errors });
 		},
