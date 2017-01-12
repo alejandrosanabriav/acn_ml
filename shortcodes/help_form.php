091ef8ff8b647a3ded3925d4086ee534eadfb8d9
Alejandro Sanabria <alesanabriav@gmail.com>
2017-01-12 16:39:41 -0500

    help form

--- shortcodes/help_form.php
+++ shortcodes/help_form.php
@@ -1,14 +1,12 @@
 <?php
 
 function bs_help_form_sc($atts, $content = null) {
-	$at = shortcode_atts( array());
-
-	  ob_start();
+	$at = shortcode_atts( array(), $atts);
+	ob_start();
 ?>
 	<div id="bs-help-form"></div>
 <?php
 	return ob_get_clean();
 }
-
 
 add_shortcode('bs_help_form', 'bs_help_form_sc');
