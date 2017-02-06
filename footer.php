952d522280071553b08ece451b2066f4bf17b878
Alejandro Sanabria <alesanabriav@gmail.com>
2017-02-06 10:35:49 -0500

    remove sidr alone

--- footer.php
+++ footer.php
@@ -52,12 +52,13 @@ if (!document.querySelector) {
 }
 </script>
 
-<script src="https://js.stripe.com/v2/"></script>
+
 <!--<script src="https://cdn.jsdelivr.net/flexslider/2.6.3/jquery.flexslider.js"></script>-->
 <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sidr/2.2.1/jquery.sidr.min.js"></script>
+<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/sidr/2.2.1/jquery.sidr.min.js"></script>-->
 	<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.1/es6-shim.js"></script>-->
-<script src="<?php echo get_template_directory_uri() . '/public/js/app.js' ?>"></script>
+  <script src="https://js.stripe.com/v2/"></script>
+  <script src="<?php echo get_template_directory_uri() . '/public/js/app.js' ?>"></script>
 <!-- End theme scripts -->
 
  <!-- Google Analytics -->
