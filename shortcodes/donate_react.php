738aa47eb2c59d257dbdb42fa0dca0582b7b9167
Alejandro Sanabria <alesanabriav@gmail.com>
2017-05-16 11:36:27 -0500

    change props

--- shortcodes/donate_react.php
+++ shortcodes/donate_react.php
@@ -2,25 +2,28 @@
 function bs_donate_react_sc($atts, $content = null) {
 	 $at = shortcode_atts([
 		 "country" => getCountry(),
-		 "other" => "Otro",
-		 "monthly" => "Mensual",
-		 "once" => "Una vez",
-		 "placeholder_amount" => "Amount",
-    "placeholder_credit_card" => "Credit Card Number",
-    "placeholder_month" => "MM",
-    "placeholder_year" => "YY",
-    "placeholder_cvc" => "CVC",
-    "placeholder_name" => "Name",
-    "placeholder_email" => "Email",
-    "placeholder_country" => "Country",
-		"validation_declined" => "Your transaction was not accepted, try again",
-    "validation_card" => "Incorrect card",
-    "validation_month" => "Incorrect month",
-    "validation_year" => "Incorrect year",
-    "validation_cvc" => "Incorrect cvc",
-    "validation_name" => "Incorrect name",
-    "validation_email" => "Incorrect email",
-    "validation_country" => "Incorrect country",
+		 "other" => gett("Other"),
+		 "monthly" => gett("Monthly"),
+		 "once" => gett("Once"),
+		 "donate" => gett("Donate"),
+		 "next" => gett("Next"),
+		 "back" => gett("Back"),
+		 "placeholder_amount" => gett("Amount"),
+    "placeholder_credit_card" => gett("Credit Card Number"),
+    "placeholder_month" => gett("MM"),
+    "placeholder_year" => gett("YY"),
+    "placeholder_cvc" => gett("CVC"),
+    "placeholder_name" => gett("Name"),
+    "placeholder_email" => gett("Email"),
+    "placeholder_country" => gett("Country"),
+		"validation_declined" => gett("Your transaction was not accepted, try again"),
+    "validation_card" => gett("Incorrect card"),
+    "validation_month" => gett("Incorrect month"),
+    "validation_year" => gett("Incorrect year"),
+    "validation_cvc" => gett("Incorrect cvc"),
+    "validation_name" => gett("Incorrect name"),
+    "validation_email" => gett("Incorrect email"),
+    "validation_country" => gett("Incorrect country"),
 		"template_uri" => str_replace("http:", "", get_template_directory_uri())
 	 ], $atts);
 
@@ -29,7 +32,13 @@ function bs_donate_react_sc($atts, $content = null) {
 
 <div 
 	class="bs-donate-react" 
-	data-props='<?php echo json_encode($at) ?>'
+	data-props='{
+    "texts": <?php echo json_encode($at) ?>, 
+    "redirect": {
+      "monthly": "<?php echo get_option('donate_monthly_redirect') ?>",
+      "once": "<?php echo get_option('donate_once_redirect') ?>"
+    }
+  }'
 >
 </div>
 
