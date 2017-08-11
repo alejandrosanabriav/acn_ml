ae12858a3fc8dc05c97a2057fcb23d93306d0ec0
Alejandro Sanabria <alesanabriav@gmail.com>
2017-08-10 21:53:22 -0500

    add convertloop track

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <?php
-  header("Access-Control-Allow-Origin: *"); 
+  header("Access-Control-Allow-Origin: *");
 ?>
 
 <!DOCTYPE html>
@@ -7,7 +7,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1, user-scalable=0">
-  
+
   <title><?php echo wp_title(); ?></title>
   <meta property="og:title" content="<?php echo get_the_title(); ?>" />
   <meta property="og:image" content="//acninternational.org/wp-content/uploads/sites/4/2016/11/slider1.jpg" />
@@ -20,7 +20,7 @@
   })(window,document,'script','dataLayer','<?php echo get_option('gta_id') ?>');</script>
   <!-- End Google Tag Manager -->
   <?php wp_head(); ?>
-  
+
   <link href="https://fonts.googleapis.com/css?family=Rubik:300,300i,400,400i,500,500i" rel="stylesheet">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/public/css/vendor.css">
   <link rel="stylesheet" href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
@@ -37,9 +37,19 @@
         window.attachEvent("onload", cb);
       } else {
         window.onload = cb;
-      } 
+      }
     }
   </script>
+
+  <!-- ConvertLoop -->
+<script>
+  !function(t,e,n,s) { t.DPEventsFunction=s,t[s]=t[s] || function() { (t[s].q=t[s].q||[]).push(arguments) }; var c=e.createElement("script"),o=e.getElementsByTagName("script")[0]; c.async=1,c.src=n,o.parentNode.insertBefore(c,o); }(window, document, "https://www.convertloop.co/v1/loop.min.js", "_dp");
+
+  _dp("configure", { appId: "746fffe4" });
+  _dp("pageView");
+</script>
+<!-- End ConvertLoop -->
+
   <!-- Start Visual Website Optimizer Asynchronous Code -->
 <script type='text/javascript'>
 var _vwo_code=(function(){
@@ -51,6 +61,8 @@ use_existing_jquery=false,
 f=false,d=document;return{use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){settings_timer=setTimeout('_vwo_code.finish()',settings_tolerance);var a=d.createElement('style'),b='body{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);this.load('//dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&r='+Math.random());return settings_timer;}};}());_vwo_settings_timer=_vwo_code.init();
 </script>
 <!-- End Visual Website Optimizer Asynchronous Code -->
+
+
 </head>
 
 <body>
